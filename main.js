3021b7ee91b2eef829fe9336548b0e8c23a6c887
ciueiCjay <rodrigo_sobral.m@hotmail.com>
2023-01-26 16:02:49 -0300

    maintenance main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function atualizarProgresso() {
     }
 }
 window.onload = function() {
-    intervalId = setInterval(atualizarProgresso, 800);
+    intervalId = setInterval(atualizarProgresso, 600);
 }
 
 
